a2472c6757b58a3d6f568b480ae158c39d0c2b64
Shabba256 <117816620+Shabba256@users.noreply.github.com>
2026-01-02 02:19:40 +0300

    r5e
    r5e

--- js/config.js
+++ js/config.js
@@ -1,19 +1,23 @@
 // --- Cloudinary ---
-const CLOUD_NAME = "YOUR_CLOUD_NAME";
-const UPLOAD_PRESET = "github_pages_upload";
+const CLOUD_NAME = "dagxhzebg";
+const UPLOAD_PRESET = "cornflakes_upload";
 
 // --- Firebase ---
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAz9xpQdNdfEBUjPAtcAONaqHJcvjKnIgE",
+  authDomain: "cornflakes-2907e.firebaseapp.com",
+  projectId: "cornflakes-2907e",
+  storageBucket: "cornflakes-2907e.appspot.com",
+  messagingSenderId: "526969299818",
+  appId: "1:526969299818:web:d30558b2cb16cb9d2afb13"
 };
+
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Services
 const db = firebase.firestore();
 const auth = firebase.auth();
 
-// --- Admin UID from Firebase Authentication ---
-const ADMIN_UID = "YOUR_ADMIN_UID";
+// Admin UID
+const ADMIN_UID = "L5DXIJbw78UUb6eXBEJ8MPvFmIf1";
